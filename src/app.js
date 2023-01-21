b9daf8920e08c3714e47d5c48026491a3b2a7e20
Renato Dardengo <rdardengo01@gmail.com>
2023-01-21 19:07:55 -0300

    Rota delete

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const fs = require ("fs");
+const methodOverride = require ("method-override")
 
 var indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
@@ -19,6 +21,7 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
 
--- src/controller/AdminController.js
+++ src/controller/AdminController.js
@@ -1,5 +1,6 @@
 const produtos = require("../database/produtos.json");
 
+
 module.exports = {
     index: (req, res) => {
             return res.render("adminListar", {
@@ -19,6 +20,7 @@ module.exports = {
         });
 
     },
+
     // Criar novo produto
     store:(req, res)=>{
         
@@ -44,17 +46,37 @@ module.exports = {
         });
 
     },
-
+    //Atualizar item no banco de dados
     update:(req, res)=>{
         //logica para atualizar
         res.redirect("/admin/produto")
     },
     //exibir a tela para mostrar o produto
     delete:(req, res)=>{
+        const { id } = req.params;
+        var produto = produtos.filter((prod) => prod.id == id);
+        produto = produto[0]
+        var arrayImg = produto.imagem;
+        var img = arrayImg[0]
+      
+        produto.imagem = img
+            return res.render('deletar-prod', {
+            produto,
+            css1: "/stylesheets/menu-footer.css",
+            css2: "/stylesheets/cadastro-prod.css",
+        });
+
 
     },
     //apagar o item do banco
     destroy:(req, res)=>{
+        // const { id } = req.params;
+        // var produto = produtos.filter((prod) => prod.id != id);
+        // const stringifyData = JSON.stringify(produto);
+        // fs.writeFileSync('produtos.json', stringifyData)
+
+        //depois que implentar faz redirect
+        res.redirect("/admin/produto")
         
 
     }
